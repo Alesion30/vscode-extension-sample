0d6ce12a2019b1b533673c6f1ae57554e2904400
Yuta Ohira <alesion90@gmail.com>
2021-07-02 20:39:21 +0900

    fix: 微調整

--- src/extension.ts
+++ src/extension.ts
@@ -8,10 +8,10 @@ export function activate(context: vscode.ExtensionContext) {
 	//////////////////////////////////////////////////////////////
 	// ステータスバー
 	//////////////////////////////////////////////////////////////
-	const inputSpeedItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
-	const charCountItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
-	const fileNameItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 3);
-	const nowTimeItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+	const inputSpeedItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1); // 入力スピード
+	const charCountItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2); // 文字数
+	const fileNameItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 3); // ファイル名
+	const nowTimeItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000); // 現在時刻
 
 
 	//////////////////////////////////////////////////////////////
@@ -52,6 +52,10 @@ export function activate(context: vscode.ExtensionContext) {
 		// 文字数反映
 		charCountItem.text = `文字数: ${count}`;
 		charCountItem.show();
+	} else {
+		// 非表示
+		fileNameItem.hide();
+		charCountItem.hide();
 	}
 	vscode.window.onDidChangeActiveTextEditor(activeEditor => {
 		if (activeEditor) {
@@ -90,7 +94,7 @@ export function activate(context: vscode.ExtensionContext) {
 	//////////////////////////////////////////////////////////////
 	const diffTime = 1; // 更新間隔[s]
 	let isTextChangeEventHookCount = 0; // ファイル編集イベント呼び出し回数
-	vscode.workspace.onDidChangeTextDocument(activeEditor => {
+	vscode.workspace.onDidChangeTextDocument(() => {
 		isTextChangeEventHookCount++;
 	}, null, context.subscriptions);
 	setInterval(() => {
@@ -99,7 +103,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const speed = diffTime > 0 ? Math.round(diffCount / diffTime * 100) / 100 : -1;
 
 		// スピード反映
-		inputSpeedItem.text = `入力スピード: ${speed}[word/s]`;
+		inputSpeedItem.text = `入力スピード: ${speed}/s`;
 		inputSpeedItem.show();
 
 		// 初期化
